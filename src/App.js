c4c50278fe6af972ccff6d381c5a1015a33a37e7
sarbamon ronghang <sarbamonronghang934@gmail.com>
2023-11-08 18:57:56 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import Todo  from "./components/Todo";
+import TodoList from "./components/TodoList"
+import Header from "./components/Header";
+import From from "./components/From";
+import { useState } from "react";
 function App() {
+  const [todo,setTodo] = useState("")
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <From 
+      todo={todo} 
+      setTodo={setTodo} 
+      todoList={todoList} 
+      setTodoList={setTodoList}
+      ></From>
+      <TodoList setTodoList={setTodoList} todoList={todoList} />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import Todo from "./Todo"
+const TodoList = ({ todoList, setTodoList }) => {
+    return (<div>
+        {todoList.map((todoItem) => (
+            <Todo
+                setTodoList={setTodoList}
+                key={todoItem.id}
+                todoItem={todoItem}
+                todoList={todoList}
+            ></Todo>
+        ))}
+    </div>
+    );
+}
+export default TodoList;
